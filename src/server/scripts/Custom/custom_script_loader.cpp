92fb88e1fdd0159fb56480b9e1cd57e33283ae3e
Kamilia Blow <windowstar24to7@gmail.com>
2024-08-19 08:08:18 +0900

    Update custom_script_loader.cpp

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -17,8 +17,8 @@
 
 // This is where scripts' loading functions should be declared:
 
-void AddSC_custom_commandscript();
-void AddSC_custom_commandscrip();
+//void AddSC_custom_commandscript();
+//void AddSC_custom_commandscrip();
 void AddSC_free_share_scripts();
 void AddSC_dragonriding_spell_scripts();
 void AddSC_CustomEffectHandler();
@@ -28,8 +28,8 @@ void AddSC_CustomEffectCommands();
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
-    AddSC_custom_commandscript();
-    AddSC_custom_commandscrip();
+    //AddSC_custom_commandscript();
+    //AddSC_custom_commandscrip();
 
     AddSC_free_share_scripts();
     AddSC_dragonriding_spell_scripts();
