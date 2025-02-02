f22b7710847c8dd7806dd2c36b7f2c8306d25bff
KamiliaBlow <windowstar24to7@gmail.com>
2025-02-02 21:33:37 +0900

    fix compile

--- src/server/game/LuaEngine/methods/TrinityCore/PlayerMethods.h
+++ src/server/game/LuaEngine/methods/TrinityCore/PlayerMethods.h
@@ -2904,9 +2904,9 @@ namespace LuaPlayer
         bool update = E->CHECKVAL<bool>(3, true);
         (void)update; // ensure that the variable is referenced in order to pass compiler checks
 
-        player->GetSpellHistory()->ResetCooldowns([category](SpellHistory::CooldownStorageType::iterator itr) -> bool
+        player->GetSpellHistory()->ResetCooldowns([category](SpellHistory::CooldownEntry const& cooldownEntry) -> bool
         {
-            SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(itr->first, DIFFICULTY_NONE);
+            SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(cooldownEntry.SpellId, DIFFICULTY_NONE);
             return spellInfo && spellInfo->GetCategory() == category;
         }, update);
 
